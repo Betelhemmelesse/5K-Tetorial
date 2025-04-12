0d662ad13c2cb58ecc52c81869040608fa5b0d44
Betelhem Melesse <167540795+Betelhemmelesse@users.noreply.github.com>
2025-04-12 16:25:05 +0300

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    N = int(input())
+    List = []
+    for _ in range(N):
+        op = input().split()
+        command = op[0]
+        if command == "print":
+            print(List)
+        elif command == "sort":
+            List.sort()
+        elif command == "pop":
+            List.pop()
+        elif command == "reverse":
+            List.reverse()
+        elif command == "append":
+            i = int(op[1])
+            List.append (i)
+        elif command == "remove":
+            i = int(op[1])
+            List.remove( i )
+        elif command == "insert":
+            i = int(op[1])
+            j = int(op[2])
+            List.insert( i, j  ) 
